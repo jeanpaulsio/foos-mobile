efef34a2643cb689154b5c0f2adfbc40030afc23
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-02-03 21:15:20 -0800

    scaffold out some components

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
 import { connect, Provider } from "react-redux";
-import { StyleSheet, Text, View } from "react-native";
+import { TextInput, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import axios from "axios";
 
 import { defaultAction } from "./actions";
 import store from "./store";
 
 class DefaultComponent extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
   componentDidMount() {
     this.props.defaultAction();
   }
+
+  handleLogin = () => {
+
+  };
 
   render() {
     return (
       <View>
         <Text>Default Component</Text>
+        <View style={styles.inputContainer}>
+          <TextInput onChangeText={email => this.setState({ email })} />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput onChangeText={password => this.setState({ password })} />
+        </View>
+        <TouchableWithoutFeedback
+          onPressIn={this.handleLogin}
+        >
+          <View>
+            <Text>Login</Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -39,5 +62,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  inputContainer: {
+    borderWidth: 1,
+  },
+  input: {
+    height: 42,
+    marginVertical: 3,
+    fontSize: 16,
   },
 });
